cd146ded8800e64a39e69bfd4c4d70e7b2359499
georuler <georuler@gmail.com>
2022-06-10 18:27:41 +0900

    laravel sabangnet package

---
+++ packages/georuler/sabangnet/src/SabangNetServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Georuler\Sabangnet;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class SabangNetServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //route add
+        Route::middleware('api')
+        ->prefix('api')
+        ->group(__DIR__ . '/routes/api.php');
+
+        //blade template path init
+        view()->addNamespace('Xml', __DIR__.'/resources/views/xml');
+
+        //config add
+        app('config')->set('sabangnet', require __DIR__ . '/config/sabangnet.php');
+    }
+}
